f9aa82056390ffee1b83e3b2d2a104dc01a2bcca
Sung Joon Park <sp3bk@virginia.edu>
2019-07-08 16:54:17 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-site/precache-manifest.fe42f226dcb7331d7e3a36be52db65a4.js"
+  "/my-site/precache-manifest.890436ceef57dabb43a735984e78f65f.js"
 );
 
 workbox.clientsClaim();
