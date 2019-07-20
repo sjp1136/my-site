799c8b17323c21ca4372c9b46ca3aba84a0023fb
Sung Joon Park <sp3bk@virginia.edu>
2019-07-19 20:07:52 -0400

    few changes

--- build/precache-manifest.7428dc211b49b48205604a3e13094637.js
+++ build/precache-manifest.7428dc211b49b48205604a3e13094637.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/my-site/static/media/shareit.92313ec2.png"
   },
   {
-    "revision": "aacc67cab826239cd609",
-    "url": "/my-site/static/css/main.e22e2cf9.chunk.css"
+    "revision": "c598910c3fa76250fccb",
+    "url": "/my-site/static/css/main.5af6a244.chunk.css"
   },
   {
     "revision": "5b5453dbaac3b8e4d9fe842ddef54642",
@@ -108,8 +108,8 @@ self.__precacheManifest = [
     "url": "/my-site/static/media/coding.2b07e1a4.jpg"
   },
   {
-    "revision": "aacc67cab826239cd609",
-    "url": "/my-site/static/js/main.aacc67ca.chunk.js"
+    "revision": "c598910c3fa76250fccb",
+    "url": "/my-site/static/js/main.c598910c.chunk.js"
   },
   {
     "revision": "9620589d21a7969b671de32a5d7a6674",
@@ -216,7 +216,7 @@ self.__precacheManifest = [
     "url": "/my-site/static/css/1.5e2e6a35.chunk.css"
   },
   {
-    "revision": "72a962dc6b8798820856ceef33a1d1a5",
+    "revision": "4f1c37515efb249fb7f214b4169ea570",
     "url": "/my-site/index.html"
   }
 ];
